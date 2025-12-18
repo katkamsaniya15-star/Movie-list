4d14fe5f865b71aad757664921d92929a1289590
Saniya Katakam <katkamsaniya15@gmail.com>
2025-12-18 16:10:18 +0530

    Describe what you changed

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,7 @@ const API_KEY = "50a8b25d";
 const BASE_URL = "https://www.omdbapi.com/";
 
 export const getPopularMovies = async () => {
-  // OMDB does NOT have popular movies
-  // We fake it by searching a common term
+
   const response = await fetch(
     `${BASE_URL}?apikey=${API_KEY}&s=avengers`
   );
